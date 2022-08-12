209c4fbfed924209e7ad655f3c5b15479ae0495f
Ming <mwang@usc.edu.au>
2022-08-12 21:32:21 +1000

    Update Main.java

--- src/au/edu/usc/Main.java
+++ src/au/edu/usc/Main.java
@@ -2,6 +2,8 @@ package au.edu.usc;
 
 public class Main {
     public static void main(String[] args) {
+        String[] files = {"file1", "file2"};
+        
         System.out.println("Starter code!");
     }
 }
